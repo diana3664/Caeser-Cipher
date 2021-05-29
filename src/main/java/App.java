6c592d4256dbfb51d45719ba8a30b617a7becbde
diana3664 <dianamus2017@gmail.com>
2021-05-29 19:50:31 +0300

    add option for encrypting and and exit option

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,23 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("This is code Caesar cipher");
+
+        for( ;; ){
+            System.out.println("choice\n 1.Encrypt\n 3.Exit");
+            int choice = scanner.nextInt();
+            Caesar caesar =new Caesar();
+            if(choice == 1){
+                System.out.println("Enter a word or sentence to Encrypt:");
+                String message = new String();
+                message = scanner.next();
+                System.out.println("Enter Key");
+                int shift = scanner.nextInt();
+                System.out.println(Caesar.encrypt(message, shift));
+            }else if (choice == 3) {
+                System.exit(0);
+            }else {
+                System.out.println("Invalid choice");
+            }
+        }
     }
 }
