15758a310067ac1f49650c94ae8e32df5eba34c6
diana3664 <dianamus2017@gmail.com>
2021-05-29 16:02:15 +0300

    add an encrypt method that takes two parameters the first being plainText and the shift key

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,4 +1,13 @@
 public class Caesar {
 
-
+    public static String encrypt(String plainText , int shift){
+            if (shift > 26){
+                shift = shift%26;
+            }
+            else if (shift < 0){
+                shift = (shift % 26)+ 26;
+            }
+            String cipherText = " ";
+            return  cipherText;
+    }
 }
--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -1,2 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class CaesarTest {
+
+//    @Test
+//    public void runCaesar_checkShiftKey_ArrayList() {
+//
+//    }
 }
