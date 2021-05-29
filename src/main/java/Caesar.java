29a051e44e43de495b286f872e39a0dd3672aa07
diana3664 <dianamus2017@gmail.com>
2021-05-29 17:10:06 +0300

    add encrypt logic to our class

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -8,6 +8,37 @@ public class Caesar {
                 shift = (shift % 26)+ 26;
             }
             String cipherText = " ";
+
+            int length = plainText.length();
+
+            for (int i = 0; i<length; i++){
+                char ch = plainText.charAt(i);
+                if(Character.isLetter(ch)){
+                    if(Character.isLowerCase(ch)){
+                        char c = (char)(ch + shift);
+                        if(c > 'z'){
+                           cipherText += (char)(ch + shift);
+                        }
+                        else {
+                            cipherText += c;
+                        }
+                    }
+                   else if(Character.isUpperCase(ch)){
+                        char c = (char)(ch + shift);
+                        if(c > 'Z'){
+                            cipherText += (char)(ch + shift);
+                        }
+                        else {
+                            cipherText += c;
+                        }
+                    }
+                }
+                else {
+                    cipherText += ch;
+                }
+            }
+
             return  cipherText;
     }
+
 }
