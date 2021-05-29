664c81b6b0c7fcb3f57014fbfa734aa38a61c12c
diana3664 <dianamus2017@gmail.com>
2021-05-29 19:28:14 +0300

    add a s.o.u.t to print our welcome message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        System.out.println("This is code Caesar cipher");
     }
 }
